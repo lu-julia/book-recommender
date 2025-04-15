d52c3b91facc220f9cfcb70514c907d48b513d03
Julia Lu <julia.lu@outlook.fr>
2025-04-15 22:13:22 +0200

    update session.py

--- src/app_utils/session.py
+++ src/app_utils/session.py
@@ -8,7 +8,10 @@ from src.data.load_data import load_rating_data, load_books_ratings
 from src.app_utils.functions import get_my_books
 
 
-def init_session_state():
+def init_session_state() -> None:
+    """
+    Initialize the session state variables used in the app.
+    """
     if "user_id" not in st.session_state:
         st.session_state["user_id"] = -1
     if "df_books" not in st.session_state and "df_user" not in st.session_state:
@@ -27,17 +30,15 @@ def init_session_state():
         st.session_state.current_page = "home"
 
     # --- Session defaults for filters ---
-    if 'search_query' not in st.session_state:
+    if "search_query" not in st.session_state:
         st.session_state.search_query = ""
-    if 'year_from' not in st.session_state:
+    if "year_from" not in st.session_state:
         st.session_state.year_from = int(st.session_state.df_books["year"].min())
-    if 'year_to' not in st.session_state:
+    if "year_to" not in st.session_state:
         st.session_state.year_to = int(st.session_state.df_books["year"].max())
-    if 'sort_by' not in st.session_state:
+    if "sort_by" not in st.session_state:
         st.session_state.sort_by = "None"
-    if 'page' not in st.session_state:
+    if "page" not in st.session_state:
         st.session_state.page = 0
     if "filters_cleared" not in st.session_state:
         st.session_state.filters_cleared = False
-
-    st.write(st.session_state)
